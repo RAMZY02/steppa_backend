b96722231bebc721128d66cc1d72a9b5dd8178f6
Luambe <melvinaudrey16@gmail.com>
2025-01-04 19:09:21 +0700

    pisah sql

--- pengepul.sql
+++ pengepul.sql
@@ -1,4 +1,31 @@
---a. Tabel suppliers (Pemasok Bahan Mentah)
+-- 1. create user admin
+CREATE USER reki IDENTIFIED BY reki
+CREATE role admin IDENTIFIED BY admin;
+GRANT CREATE TABLE TO admin;
+GRANT CREATE INDEX TO admin;
+GRANT CREATE TRIGGER TO admin;
+GRANT CREATE VIEW TO admin;
+GRANT CREATE PROCEDURE TO admin;
+GRANT GRANT ANY OBJECT PRIVILEGE TO admin;
+GRANT ALTER ANY TABLE TO admin;
+GRANT CREATE SEQUENCE TO admin;
+GRANT CREATE ROLE TO admin;
+ALTER USER admin QUOTA UNLIMITED ON USERS;
+GRANT ADMIN TO reki;
+
+
+--login ke reki
+conn reki@steppa_store/reki
+
+
+
+--role admin
+set role admin identified by admin
+
+
+
+
+--create table
 CREATE TABLE suppliers (
     supplier_id VARCHAR2(10)  PRIMARY KEY,
     name VARCHAR2(100) NOT NULL,
@@ -7,7 +34,6 @@ CREATE TABLE suppliers (
     is_deleted CHAR(1) DEFAULT 'N'
 );
 
---b. Tabel raw_materials (Stok Bahan Mentah)
 CREATE TABLE raw_materials (
     material_id VARCHAR2(10)  PRIMARY KEY,
     material_name VARCHAR2(100) NOT NULL,
@@ -337,256 +363,5 @@ BEGIN
         INSERT INTO log_pengepul (log_id, action_type, table_name, action_details, action_user)
         VALUES (log_pengepul_seq.NEXTVAL, 'U', 'transaction_detail', v_action_details, v_user);
     END IF;
-END;
-/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
---d. Tabel finished_goods (Stok Barang Jadi)
-CREATE TABLE finished_goods (
-    product_id VARCHAR2(10)  PRIMARY KEY,
-    stock_quantity NUMBER NOT NULL CHECK (stock_quantity >= 0),
-    warehouse_location VARCHAR2(100),
-    last_update DATE DEFAULT SYSDATE
-);
-
---e. Tabel stores (Gerai Toko)
-CREATE TABLE stores (
-    store_id VARCHAR2(10)  PRIMARY KEY,
-    store_name VARCHAR2(100) NOT NULL,
-    location VARCHAR2(100) NOT NULL
-);
-
-
-
-
-
-
-
-
-
-
-
-
-
-CREATE TABLE products (
-    product_id VARCHAR2(10) PRIMARY KEY,
-    product_name VARCHAR2(100) NOT NULL,
-    product_description VARCHAR2(255),
-    product_category VARCHAR2(50) NOT NULL,
-    stok_qty NUMBER NOT NULL,
-    price NUMBER NOT NULL,
-    is_deleted CHAR(1) DEFAULT 'N'
-);
-
-CREATE TABLE sales (
-    sale_id VARCHAR2(10) PRIMARY KEY,
-    sale_channel VARCHAR2(50) NOT NULL,
-    sale_date DATE NOT NULL,
-    total NUMBER NOT NULL,
-    is_deleted CHAR(1) DEFAULT 'N'
-);
-
-CREATE TABLE sale_items (
-    sale_item_id VARCHAR2(10) PRIMARY KEY,
-    sale_id VARCHAR2(10) NOT NULL REFERENCES sales(sale_id),
-    product_id VARCHAR2(10) NOT NULL REFERENCES products(product_id),
-    quantity NUMBER NOT NULL,
-    price NUMBER NOT NULL,
-    subtotal NUMBER NOT NULL,
-    is_deleted CHAR(1) DEFAULT 'N'
-);
-
-CREATE TABLE customers (
-    customer_id VARCHAR2(10) PRIMARY KEY,
-    name VARCHAR2(100) NOT NULL,
-    email VARCHAR2(100),
-    phone_number VARCHAR2(15),
-    address VARCHAR2(200),
-    city VARCHAR2(50),
-    country VARCHAR2(50),
-    zip_code VARCHAR2(10),
-    is_deleted CHAR(1) DEFAULT 'N'
-);
-
-CREATE TABLE carts (
-    cart_id VARCHAR2(10) PRIMARY KEY,
-    customer_id VARCHAR2(10) NOT NULL REFERENCES customers(customer_id),
-    is_deleted CHAR(1) DEFAULT 'N'
-);
-
-CREATE TABLE cart_items (
-    cart_item_id VARCHAR2(10) PRIMARY KEY,
-    cart_id VARCHAR2(10) NOT NULL REFERENCES carts(cart_id),
-    product_id VARCHAR2(10) NOT NULL REFERENCES products(product_id),
-    quantity NUMBER NOT NULL CHECK (quantity > 0),
-    price NUMBER NOT NULL,
-    status VARCHAR2(20) DEFAULT 'active',
-    added_at DATE DEFAULT SYSDATE,
-    is_deleted CHAR(1) DEFAULT 'N'
-);
-
-CREATE TABLE revenue_reports (
-    report_id VARCHAR2(10) PRIMARY KEY,
-    report_period DATE NOT NULL,
-    total_revenue NUMBER NOT NULL,
-    total_expenses NUMBER NOT NULL,
-    net_profit NUMBER NOT NULL,
-    is_deleted CHAR(1) DEFAULT 'N'
-);
-
-
-
-
-
-CREATE SEQUENCE seq_products_id
-START WITH 1 
-INCREMENT BY 1 
-MINVALUE 1 
-MAXVALUE 9999 
-NOCYCLE 
-CACHE 10;
-
-CREATE OR REPLACE TRIGGER trg_bef_products
-BEFORE INSERT ON products
-FOR EACH ROW
-BEGIN
-    :NEW.product_id := 'PRO' || LPAD(seq_products_id.NEXTVAL, 4, '0');
-END;
-/
-
-
-
-CREATE SEQUENCE seq_sales_id
-START WITH 1 
-INCREMENT BY 1 
-MINVALUE 1 
-MAXVALUE 9999 
-NOCYCLE 
-CACHE 10;
-
-CREATE OR REPLACE TRIGGER trg_bef_sales
-BEFORE INSERT ON sales
-FOR EACH ROW
-BEGIN
-    :NEW.sale_id := 'SLS' || LPAD(seq_sales_id.NEXTVAL, 4, '0');
-END;
-/
-
-
-
-CREATE SEQUENCE seq_sale_items_id
-START WITH 1 
-INCREMENT BY 1 
-MINVALUE 1 
-MAXVALUE 9999 
-NOCYCLE 
-CACHE 10;
-
-CREATE OR REPLACE TRIGGER trg_bef_sale_items
-BEFORE INSERT ON sale_items
-FOR EACH ROW
-BEGIN
-    :NEW.sale_item_id := 'SLI' || LPAD(seq_sale_items_id.NEXTVAL, 4, '0');
-END;
-/
-
-
-
-CREATE SEQUENCE seq_customers_id
-START WITH 1 
-INCREMENT BY 1 
-MINVALUE 1 
-MAXVALUE 9999 
-NOCYCLE 
-CACHE 10;
-
-CREATE OR REPLACE TRIGGER trg_bef_customers
-BEFORE INSERT ON customers
-FOR EACH ROW
-BEGIN
-    :NEW.customer_id := 'CUS' || LPAD(seq_customers_id.NEXTVAL, 4, '0');
-END;
-/
-
-
-
-CREATE SEQUENCE seq_carts_id
-START WITH 1 
-INCREMENT BY 1 
-MINVALUE 1 
-MAXVALUE 9999 
-NOCYCLE 
-CACHE 10;
-
-CREATE OR REPLACE TRIGGER trg_bef_carts
-BEFORE INSERT ON carts
-FOR EACH ROW
-BEGIN
-    :NEW.cart_id := 'CRT' || LPAD(seq_carts_id.NEXTVAL, 4, '0');
-END;
-/
-
-
-CREATE SEQUENCE seq_cart_items_id
-START WITH 1 
-INCREMENT BY 1 
-MINVALUE 1 
-MAXVALUE 9999 
-NOCYCLE 
-CACHE 10;
-
-CREATE OR REPLACE TRIGGER trg_bef_cart_items
-BEFORE INSERT ON cart_items
-FOR EACH ROW
-BEGIN
-    :NEW.cart_id := 'CIT' || LPAD(seq_cart_items_id.NEXTVAL, 4, '0');
-END;
-/
-
-
-CREATE SEQUENCE seq_revenue_id
-START WITH 1 
-INCREMENT BY 1 
-MINVALUE 1 
-MAXVALUE 9999 
-NOCYCLE 
-CACHE 10;
-
-CREATE OR REPLACE TRIGGER trg_bef_revenue
-BEFORE INSERT ON revenue_reports
-FOR EACH ROW
-BEGIN
-    :NEW.report_id := 'REV' || LPAD(seq_revenue_id.NEXTVAL, 4, '0');
 END;
 /
---
+++ store.sql
@@ -0,0 +1,219 @@
+-- 1. create user admin
+CREATE USER reki IDENTIFIED BY reki
+CREATE role admin IDENTIFIED BY admin;
+GRANT CREATE TABLE TO admin;
+GRANT CREATE INDEX TO admin;
+GRANT CREATE TRIGGER TO admin;
+GRANT CREATE VIEW TO admin;
+GRANT CREATE PROCEDURE TO admin;
+GRANT GRANT ANY OBJECT PRIVILEGE TO admin;
+GRANT ALTER ANY TABLE TO admin;
+GRANT CREATE SEQUENCE TO admin;
+GRANT CREATE ROLE TO admin;
+ALTER USER admin QUOTA UNLIMITED ON USERS;
+GRANT ADMIN TO reki;
+
+
+--login ke reki
+conn reki@steppa_store/reki
+
+
+
+--role admin
+set role admin identified by admin
+
+
+
+
+--create table
+CREATE TABLE products (
+    product_id VARCHAR2(10) PRIMARY KEY,
+    product_name VARCHAR2(100) NOT NULL,
+    product_description VARCHAR2(255),
+    product_category VARCHAR2(50) NOT NULL,
+    stok_qty NUMBER NOT NULL,
+    price NUMBER NOT NULL,
+    is_deleted CHAR(1) DEFAULT 'N'
+);
+
+CREATE TABLE sales (
+    sale_id VARCHAR2(10) PRIMARY KEY,
+    sale_channel VARCHAR2(50) NOT NULL,
+    sale_date DATE NOT NULL,
+    total NUMBER NOT NULL,
+    is_deleted CHAR(1) DEFAULT 'N'
+);
+
+CREATE TABLE sale_items (
+    sale_item_id VARCHAR2(10) PRIMARY KEY,
+    sale_id VARCHAR2(10) NOT NULL REFERENCES sales(sale_id),
+    product_id VARCHAR2(10) NOT NULL REFERENCES products(product_id),
+    quantity NUMBER NOT NULL,
+    price NUMBER NOT NULL,
+    subtotal NUMBER NOT NULL,
+    is_deleted CHAR(1) DEFAULT 'N'
+);
+
+CREATE TABLE customers (
+    customer_id VARCHAR2(10) PRIMARY KEY,
+    name VARCHAR2(100) NOT NULL,
+    email VARCHAR2(100),
+    phone_number VARCHAR2(15),
+    address VARCHAR2(200),
+    city VARCHAR2(50),
+    country VARCHAR2(50),
+    zip_code VARCHAR2(10),
+    is_deleted CHAR(1) DEFAULT 'N'
+);
+
+CREATE TABLE carts (
+    cart_id VARCHAR2(10) PRIMARY KEY,
+    customer_id VARCHAR2(10) NOT NULL REFERENCES customers(customer_id),
+    is_deleted CHAR(1) DEFAULT 'N'
+);
+
+CREATE TABLE cart_items (
+    cart_item_id VARCHAR2(10) PRIMARY KEY,
+    cart_id VARCHAR2(10) NOT NULL REFERENCES carts(cart_id),
+    product_id VARCHAR2(10) NOT NULL REFERENCES products(product_id),
+    quantity NUMBER NOT NULL CHECK (quantity > 0),
+    price NUMBER NOT NULL,
+    status VARCHAR2(20) DEFAULT 'active',
+    added_at DATE DEFAULT SYSDATE,
+    is_deleted CHAR(1) DEFAULT 'N'
+);
+
+CREATE TABLE revenue_reports (
+    report_id VARCHAR2(10) PRIMARY KEY,
+    report_period DATE NOT NULL,
+    total_revenue NUMBER NOT NULL,
+    total_expenses NUMBER NOT NULL,
+    net_profit NUMBER NOT NULL,
+    is_deleted CHAR(1) DEFAULT 'N'
+);
+
+
+
+
+
+CREATE SEQUENCE seq_products_id
+START WITH 1 
+INCREMENT BY 1 
+MINVALUE 1 
+MAXVALUE 9999 
+NOCYCLE 
+CACHE 10;
+
+CREATE OR REPLACE TRIGGER trg_bef_products
+BEFORE INSERT ON products
+FOR EACH ROW
+BEGIN
+    :NEW.product_id := 'PRO' || LPAD(seq_products_id.NEXTVAL, 4, '0');
+END;
+/
+
+
+
+CREATE SEQUENCE seq_sales_id
+START WITH 1 
+INCREMENT BY 1 
+MINVALUE 1 
+MAXVALUE 9999 
+NOCYCLE 
+CACHE 10;
+
+CREATE OR REPLACE TRIGGER trg_bef_sales
+BEFORE INSERT ON sales
+FOR EACH ROW
+BEGIN
+    :NEW.sale_id := 'SLS' || LPAD(seq_sales_id.NEXTVAL, 4, '0');
+END;
+/
+
+
+
+CREATE SEQUENCE seq_sale_items_id
+START WITH 1 
+INCREMENT BY 1 
+MINVALUE 1 
+MAXVALUE 9999 
+NOCYCLE 
+CACHE 10;
+
+CREATE OR REPLACE TRIGGER trg_bef_sale_items
+BEFORE INSERT ON sale_items
+FOR EACH ROW
+BEGIN
+    :NEW.sale_item_id := 'SLI' || LPAD(seq_sale_items_id.NEXTVAL, 4, '0');
+END;
+/
+
+
+
+CREATE SEQUENCE seq_customers_id
+START WITH 1 
+INCREMENT BY 1 
+MINVALUE 1 
+MAXVALUE 9999 
+NOCYCLE 
+CACHE 10;
+
+CREATE OR REPLACE TRIGGER trg_bef_customers
+BEFORE INSERT ON customers
+FOR EACH ROW
+BEGIN
+    :NEW.customer_id := 'CUS' || LPAD(seq_customers_id.NEXTVAL, 4, '0');
+END;
+/
+
+
+
+CREATE SEQUENCE seq_carts_id
+START WITH 1 
+INCREMENT BY 1 
+MINVALUE 1 
+MAXVALUE 9999 
+NOCYCLE 
+CACHE 10;
+
+CREATE OR REPLACE TRIGGER trg_bef_carts
+BEFORE INSERT ON carts
+FOR EACH ROW
+BEGIN
+    :NEW.cart_id := 'CRT' || LPAD(seq_carts_id.NEXTVAL, 4, '0');
+END;
+/
+
+
+CREATE SEQUENCE seq_cart_items_id
+START WITH 1 
+INCREMENT BY 1 
+MINVALUE 1 
+MAXVALUE 9999 
+NOCYCLE 
+CACHE 10;
+
+CREATE OR REPLACE TRIGGER trg_bef_cart_items
+BEFORE INSERT ON cart_items
+FOR EACH ROW
+BEGIN
+    :NEW.cart_id := 'CIT' || LPAD(seq_cart_items_id.NEXTVAL, 4, '0');
+END;
+/
+
+
+CREATE SEQUENCE seq_revenue_id
+START WITH 1 
+INCREMENT BY 1 
+MINVALUE 1 
+MAXVALUE 9999 
+NOCYCLE 
+CACHE 10;
+
+CREATE OR REPLACE TRIGGER trg_bef_revenue
+BEFORE INSERT ON revenue_reports
+FOR EACH ROW
+BEGIN
+    :NEW.report_id := 'REV' || LPAD(seq_revenue_id.NEXTVAL, 4, '0');
+END;
+/
