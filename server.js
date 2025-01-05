5acf632f7f01847da8760b15f6195c069b1edfe7
Luambe <melvinaudrey16@gmail.com>
2025-01-05 15:52:47 +0700

    .

--- server.js
+++ server.js
@@ -7,10 +7,11 @@ const pengepulRoutes = require("./src/routes/pengepulRoutes");
 const storeRoutes = require("./src/routes/storeRoutes");
 const rndRoutes = require("./src/routes/rndRoutes");
 
-app.use(cors({
-  origin: "http://localhost:3000", // Atau URL frontend Anda
-}));
-
+app.use(
+  cors({
+    origin: "*", // Izinkan semua domain
+  })
+);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -22,5 +23,5 @@ app.use("/api/rnd", rndRoutes);
 
 // Mulai server
 app.listen(port, () => {
-  console.log(`Server berjalan di http://192.168.18.18:${port}`);
+  console.log(`Server berjalan di http://192.168.1.6:${port}`);
 });
